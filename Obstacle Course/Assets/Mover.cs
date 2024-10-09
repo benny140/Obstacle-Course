8ccf267a00f6f7fa54ada9c270ed9bd25ac7fa2f
bguo <benjaminguo14@gmail.com>
2024-10-09 23:59:38 +1100

    up to 21

--- Obstacle Course/Assets/Mover.cs
+++ Obstacle Course/Assets/Mover.cs
@@ -8,10 +8,23 @@ public class Mover : MonoBehaviour
     float moveSpeed = 10f;
 
     // Start is called before the first frame update
-    void Start() { }
+    void Start()
+    {
+        PrintInstructions();
+    }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Move with WASD or Arrow Keys");
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
